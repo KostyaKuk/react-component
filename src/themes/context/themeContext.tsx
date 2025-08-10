d5c6aeef1d59283397cb79d3d6cadfc1f84a714a
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2025-08-10 23:06:36 +0300

    feat: Create deploy #2

--- src/themes/context/themeContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import type { ThemeContextType } from './themeProvider';
-
-export const ThemeContext = createContext<ThemeContextType | undefined>(
-  undefined
-);
